58c93d74daccdafa9a4d1d68e70b4520e5029b68
standardversion <standardversion@tuta.io>
2025-04-07 09:34:51 -0700

    Adds corresponding cucumber scenarios for the unit tests

--- tests/tuple_tests.cpp
+++ tests/tuple_tests.cpp
@@ -1,9 +1,20 @@
-#include <string>
+﻿#include <string>
 #include <vector>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../tuple.h"
 
+
+/*
+Scenario: A tuple with w=1.0 is a point
+  Given a ← tuple(4.3, -4.2, 3.1, 1.0)
+  Then a.x = 4.3
+	And a.y = -4.2
+	And a.z = 3.1
+	And a.w = 1.0
+	And a is a point
+	And a is not a vector
+*/
 TEST(tuple, should_be_a_point_if_w_is_1_dot_0)
 {
 	const tuple_t a{ 4.3, -4.2, 3.1, 1.0 };
@@ -15,6 +26,16 @@ TEST(tuple, should_be_a_point_if_w_is_1_dot_0)
 	EXPECT_FALSE(a.is_vector());
 }
 
+/*
+Scenario: A tuple with w=0 is a vector
+  Given a ← tuple(4.3, -4.2, 3.1, 0.0)
+  Then a.x = 4.3
+	And a.y = -4.2
+	And a.z = 3.1
+	And a.w = 0.0
+	And a is not a point
+	And a is a vector
+*/
 TEST(tuple, should_be_a_vector_if_w_is_0)
 {
 	const tuple_t a{ 4.3, -4.2, 3.1, 0.0 };
@@ -26,6 +47,11 @@ TEST(tuple, should_be_a_vector_if_w_is_0)
 	EXPECT_TRUE(a.is_vector());
 }
 
+/*
+Scenario: point() creates tuples with w=1
+  Given p ← point(4, -4, 3)
+  Then p = tuple(4, -4, 3, 1)
+*/
 TEST(tuple, should_create_a_tuple_using_point_static_func)
 {
 	const tuple_t p_point{ tuple_t::point(4, -4, 3) };
@@ -36,6 +62,11 @@ TEST(tuple, should_create_a_tuple_using_point_static_func)
 	EXPECT_EQ(p_point.w, p_tuple.w);
 }
 
+/*
+Scenario: vector() creates tuples with w=0
+  Given v ← vector(4, -4, 3)
+  Then v = tuple(4, -4, 3, 0)
+*/
 TEST(tuple, should_create_a_tuple_using_vector_static_func)
 {
 	const tuple_t v_vector{ tuple_t::vector(4, -4, 3) };
@@ -46,6 +77,14 @@ TEST(tuple, should_create_a_tuple_using_vector_static_func)
 	EXPECT_EQ(v_vector.w, v_tuple.w);
 }
 
+/*
+Scenario: Comparing two tuples
+  Given a ← tuple(4.3, -4.2, 3.1, 0.0)
+  And b ← tuple(4.3, -4.2, 3.1, 1.0)
+  And c ← tuple(4.3, -4.2, 3.1, 0.0)
+  Then a != b
+  And a = c
+*/
 TEST(tuple, should_implement_comparison_operator)
 {
 	const tuple_t a{ 4.3, -4.2, 3.1, 0.0 };
@@ -55,6 +94,12 @@ TEST(tuple, should_implement_comparison_operator)
 	EXPECT_TRUE(a == c);
 }
 
+/*
+Scenario: Adding two tuples
+  Given a ← tuple(3, -2, 5, 1)
+	And b ← tuple(-2, 3, 1, 0)
+   Then a + ab = tuple(1, 1, 6, 1)
+*/
 TEST(tuple, should_implement_addition_operator)
 {
 	const tuple_t a{ 3, -2, 5, 1 };
@@ -64,6 +109,12 @@ TEST(tuple, should_implement_addition_operator)
 	EXPECT_EQ(c, expected);
 }
 
+/*
+Scenario: Adding two points
+  Given a1 ← tuple(3, -2, 5, 1)
+	And a2 ← tuple(-2, 3, 1, 1)
+   Then a1 + a2 throws invalid argument exception
+*/
 TEST(tuple, should_throw_invalid_arg_when_adding_two_points)
 {
 	const tuple_t a{ 3, -2, 5, 1 };
@@ -71,6 +122,12 @@ TEST(tuple, should_throw_invalid_arg_when_adding_two_points)
 	EXPECT_THROW(a + b, std::invalid_argument);
 }
 
+/*
+Scenario: Subtracting two points
+  Given p1 ← point(3, 2, 1)
+	And p2 ← point(5, 6, 7)
+  Then p1 - p2 = vector(-2, -4, -6)
+*/
 TEST(tuple, should_implement_subtraction_operator_two_points_gives_a_vector)
 {
 	const tuple_t p1{ tuple_t::point(3, 2, 1) };
@@ -80,6 +137,12 @@ TEST(tuple, should_implement_subtraction_operator_two_points_gives_a_vector)
 	EXPECT_TRUE(v.is_vector());
 }
 
+/*
+Scenario: Subtracting a vector from a point
+  Given p ← point(3, 2, 1)
+	And v ← vector(5, 6, 7)
+  Then p - v = point(-2, -4, -6)
+*/
 TEST(tuple, should_result_is_a_point_when_vector_is_subtracted_from_a_point)
 {
 	const tuple_t p{ tuple_t::point(3, 2, 1) };
@@ -89,6 +152,12 @@ TEST(tuple, should_result_is_a_point_when_vector_is_subtracted_from_a_point)
 	EXPECT_TRUE(r.is_point());
 }
 
+/*
+Scenario: Subtracting two vectors
+  Given v1 ← vector(3, 2, 1)
+	And v2 ← vector(5, 6, 7)
+  Then v1 - v2 = vector(-2, -4, -6)
+*/
 TEST(tuple, should_result_is_a_vector_when_two_vectors_are_subtracted)
 {
 	const tuple_t v1{ tuple_t::vector(3, 2, 1) };
